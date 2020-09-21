adc973dd4e7f6bdb6c0d0f416854c14616209577
Leonardo Costa <leonardoeverton.tec@gmail.com>
2020-09-20 22:11:41 -0300

    9. Angular Fundamentos -- 9.8. Usando variável de referência

--- meuprojeto/src/app/app.component.ts
+++ meuprojeto/src/app/app.component.ts
@@ -8,16 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   nome = 'Leonardo Costa';
 
-  adicionar() {
-    console.log(`Adicionando ${this.nome}`);
-
-    const numero = Math.round(Math.random() * 100);
-    this.nome = 'Leoanrdo ' + numero;
-  }
-
-  alterarNome(event: any) {
-    // console.log(event);
-    this.nome = event.target.value;
+  adicionar(nome) {
+    this.nome = nome;
   }
 
 }
