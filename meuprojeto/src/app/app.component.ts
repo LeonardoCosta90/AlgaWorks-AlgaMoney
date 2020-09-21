a84c8892c72d3bbbd2565a537798b2b3586b27ae
Leonardo Costa <leonardoeverton.tec@gmail.com>
2020-09-20 22:17:24 -0300

    9. Angular Fundamentos -- 9.9. Usando property binding

--- meuprojeto/src/app/app.component.ts
+++ meuprojeto/src/app/app.component.ts
@@ -1,3 +1,4 @@
+  
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,8 +9,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   nome = 'Leonardo Costa';
 
-  adicionar(nome) {
-    this.nome = nome;
+  adicionar() {
+    console.log(`Adicionando ${this.nome}`);
+
+    const numero = Math.round(Math.random() * 100);
+    this.nome = 'João ' + numero;
+  }
+
+  alterarNome(event: any) {
+    this.nome = event.target.value;
   }
 
 }
