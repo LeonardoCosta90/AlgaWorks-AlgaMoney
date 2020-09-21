ae2a96db6ddb8bff31c13e4b32cc9dacd8fc52c3
Leonardo Costa <leonardoeverton.tec@gmail.com>
2020-09-20 23:42:28 -0300

    9.Angular Fundamentos -- 9.17. Estilos CSS dinâmicos com ngStyle

--- meuprojeto/src/app/funcionario-form/funcionario-form.component.ts
+++ meuprojeto/src/app/funcionario-form/funcionario-form.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class FuncionarioFormComponent {
 
   ultimoId = 0;
-  nome = 'Leoanrdo';
+  nome = 'Leonardo';
   adicionado = false;
   @Output('criado') funcionarioAdicionado = new EventEmitter();
 
