2f583d08e179840496a54f01d52eb460fe28ba80
Leonardo Costa <leonardoeverton.tec@gmail.com>
2020-10-27 13:16:02 -0300

    22.30. Configurando o serviço S3

--- algamoney-api/src/main/java/com/example/algamoney/api/config/property/AlgamoneyApiProperty.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/config/property/AlgamoneyApiProperty.java
@@ -8,6 +8,18 @@ public class AlgamoneyApiProperty {
 	private String originPermitida = "http://localhost:8000";
 
 	private final Seguranca seguranca = new Seguranca();
+	
+	private final Mail mail = new Mail();
+	
+	private final S3 s3 = new S3();
+	
+	public S3 getS3() {
+		return s3;
+	}
+	
+	public Mail getMail() {
+		return mail;
+	}
 
 	public Seguranca getSeguranca() {
 		return seguranca;
@@ -19,6 +31,29 @@ public class AlgamoneyApiProperty {
 
 	public void setOriginPermitida(String originPermitida) {
 		this.originPermitida = originPermitida;
+	}
+	
+	public static class S3 {
+		
+		private String accessKeyId;
+		
+		private String secretAccessKey;
+
+		public String getAccessKeyId() {
+			return accessKeyId;
+		}
+
+		public void setAccessKeyId(String accessKeyId) {
+			this.accessKeyId = accessKeyId;
+		}
+
+		public String getSecretAccessKey() {
+			return secretAccessKey;
+		}
+
+		public void setSecretAccessKey(String secretAccessKey) {
+			this.secretAccessKey = secretAccessKey;
+		}
 	}
 
 	public static class Seguranca {
@@ -32,7 +67,48 @@ public class AlgamoneyApiProperty {
 		public void setEnableHttps(boolean enableHttps) {
 			this.enableHttps = enableHttps;
 		}
-
 	}
 
+	public static class Mail {
+		
+		private String host;
+		
+		private Integer port;
+		
+		private String username;
+		
+		private String password;
+
+		public String getHost() {
+			return host;
+		}
+
+		public void setHost(String host) {
+			this.host = host;
+		}
+
+		public Integer getPort() {
+			return port;
+		}
+
+		public void setPort(Integer port) {
+			this.port = port;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+	}
 }
