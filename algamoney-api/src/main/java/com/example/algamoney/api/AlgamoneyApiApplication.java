2efe75062c50f9ea97241e2df65a09aec6609ce0
Leonardo Costa <leonardoeverton.tec@gmail.com>
2020-10-27 18:07:42 -0300

    22.36. Configurando URL do anexo

--- algamoney-api/src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
@@ -3,6 +3,7 @@ package com.example.algamoney.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 
 import com.example.algamoney.api.config.property.AlgamoneyApiProperty;
 
@@ -10,8 +11,13 @@ import com.example.algamoney.api.config.property.AlgamoneyApiProperty;
 @EnableConfigurationProperties(AlgamoneyApiProperty.class)
 public class AlgamoneyApiApplication {
 
+	private static ApplicationContext APPLICATIONCONTEXT;
+	
 	public static void main(String[] args) {
-		SpringApplication.run(AlgamoneyApiApplication.class, args);
+		APPLICATIONCONTEXT = SpringApplication.run(AlgamoneyApiApplication.class, args);
 	}
 	
+	public static <T> T getBean(Class<T> type){
+		return APPLICATIONCONTEXT.getBean(type);
+	}
 }
